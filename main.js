af67bff67624eb818a7cf80a3ce4994d1592dbdd
mpirunthapan <mpirunthapan@gmail.com>
2020-01-02 21:37:38 +0530

    Uki6funwork1

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
-$(document).ready(function() {  
-SC.stream('/tracks/201507857',function(sound){ $('#start').click(function(e) {
+$(document).ready(function() {
+   SC.stream('/tracks/201507857',function(sound){
+     $('#start').click(function(e) {
            e.preventDefault();
            sound.start();
          });
-$('#stop').click(function(e) {
+         $('#stop').click(function(e) {
            e.preventDefault();
            sound.stop();
          });
-
-});
+   });
 });
