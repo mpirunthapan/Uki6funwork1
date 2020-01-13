4b1cbe72c5315139c63653fde2666828abdbf026
mpirunthapan <mpirunthapan@gmail.com>
2020-01-13 12:44:31 +0530

    funwork1

--- main.js
+++ main.js
@@ -1,8 +1,202 @@
+<!--titanic theme song-->
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 $(document).ready(function() {
    SC.stream('/tracks/201507857',function(sound){
+     $('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop1').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+<!--padi padi leechu manasu-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/536302422',function(sound){
+     $('#start2').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop2').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+<!--samjavaragamana-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/687936454',function(sound){
+     $('#start3').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop3').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+<!--taki taki-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/512660685',function(sound){
+     $('#start4').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop4').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+<!--dumb dumb-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/379584962',function(sound){
+     $('#start5').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop5').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+<!--maruvarthai-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/498231915',function(sound){
+     $('#start6').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop6').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+
+<!--maate vinadhuga-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/522633336',function(sound){
+     $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+
+<!--pularaadha-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/705912043',function(sound){
+     $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+
+<!--inkem inkem inkem-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/476606349',function(sound){
+     $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+
+<!--ellu vaya pookalaye-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/717280654',function(sound){
+     $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+
+<!--verithanam-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/665105183',function(sound){
+     $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+         $('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+   });
+});
+
+
+<!--thaarame-->
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+   SC.stream('/tracks/671766368',function(sound){
      $('#start').click(function(e) {
            e.preventDefault();
            sound.start();
